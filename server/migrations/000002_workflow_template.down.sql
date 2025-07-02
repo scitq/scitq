f358cb4f53fe9d1621dfbfd74022e7ea058f66f8
Raynald de Lahondes <raynald@delahondes.com>
2025-07-02 09:21:23 +0200

    fix cascade in DB schema

--- server/migrations/000002_workflow_template.down.sql
+++ server/migrations/000002_workflow_template.down.sql
@@ -2,4 +2,17 @@ DROP TABLE IF EXISTS template_run;
 DROP TABLE IF EXISTS workflow_template;
 ALTER TABLE task DROP COLUMN task_name;
 
+-- unfix recruiter cascade
+ALTER TABLE recruiter
+DROP CONSTRAINT recruiter_step_id_fkey;
+ALTER TABLE recruiter
+ADD CONSTRAINT recruiter_step_id_fkey
+FOREIGN KEY (step_id) REFERENCES step(step_id);
+
+-- unfix task cascade
+ALTER TABLE task DROP CONSTRAINT IF EXISTS task_step_id_fkey;
+ALTER TABLE task
+ADD CONSTRAINT task_step_id_fkey
+FOREIGN KEY (step_id) REFERENCES step(step_id) ON DELETE SET NULL;
+
 DROP INDEX IF EXISTS idx_task_dependencies_by_prerequisite;
--- server/migrations/000002_workflow_template.up.sql
+++ server/migrations/000002_workflow_template.up.sql
@@ -21,5 +21,18 @@ CREATE TABLE template_run (
 
 ALTER TABLE task ADD COLUMN task_name TEXT;
 
+-- fix recruiter cascade
+ALTER TABLE recruiter
+DROP CONSTRAINT recruiter_step_id_fkey;
+ALTER TABLE recruiter
+ADD CONSTRAINT recruiter_step_id_fkey
+FOREIGN KEY (step_id) REFERENCES step(step_id) ON DELETE CASCADE;
+
+-- fix task cascade
+ALTER TABLE task DROP CONSTRAINT IF EXISTS task_step_id_fkey;
+ALTER TABLE task
+ADD CONSTRAINT task_step_id_fkey
+FOREIGN KEY (step_id) REFERENCES step(step_id) ON DELETE CASCADE;
+
 CREATE INDEX idx_task_dependencies_by_prerequisite
 ON task_dependencies (prerequisite_task_id);
