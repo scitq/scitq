4522bee9921f387a93e59d1167c500328a418899
Raynald de Lahondes <raynald@delahondes.com>
2025-10-29 09:28:22 +0100

    Place SCITQ logo everywhere.

--- ui/src/pages/LoginPage.svelte
+++ ui/src/pages/LoginPage.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount } from 'svelte';
   import LoginForm from '../components/LoginForm.svelte';
-  import logo from '../assets/icons/logoGMT.png';
+  import logo from '../assets/icons/logoSCITQ2.png';
   import '../styles/loginPage.css';
 </script>
 
@@ -18,7 +18,7 @@
   <div class="loginpage-header_login">
     <div class="top-left">SCITQ2</div>
     <div class="loginpage-top-right">
-      <span>GMT SCIENCE</span>
+      <span></span>
       <img src="{logo}" alt="Logo" class="loginpage-login-page-logo" />
     </div>
   </div>
